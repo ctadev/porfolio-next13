e2e92b5fcddf3d4a888d6188186603ac86376151
ctadev <ctakammy@gmail.com>
2023-04-11 10:25:41 -0400

    test paralax

--- app/(components)/Hero.jsx
+++ app/(components)/Hero.jsx
@@ -5,7 +5,7 @@ import Plx from "react-plx";
 
 const Hero = () => {
   return (
-    <main className="bg-black relative">
+    <main className=" relative">
       <Plx
         parallaxData={[
           {
@@ -26,7 +26,7 @@ const Hero = () => {
           left: 0,
           top: 0,
           width: "100%",
-          zIndex: 100,
+          zIndex: -1,
         }}
       >
         <img style={{ width: "100%" }} src="grass.png" alt="foreground" />
@@ -50,60 +50,11 @@ const Hero = () => {
           left: 0,
           top: 0,
           width: "100%",
-          height: "100VH",
+          zIndex: 3,
         }}
       >
         <img style={{ width: "100%" }} src="background.jpg" alt="background" />
       </Plx>
-      {/* <Plx
-        parallaxData={[
-          {
-            start: 0,
-            end: 400,
-            properties: [
-              {
-                startValue: 1,
-                endValue: 0,
-                property: "opacity",
-              },
-            ],
-          },
-        ]}
-        style={{
-          position: "sticky",
-          left: "50%",
-          right: "50%",
-          margin: "auto",
-          top: "26vw",
-          width: "100%",
-        }}
-      >
-        <img
-          style={{
-            width: "30vw",
-          }}
-          src="/text-img.webp"
-          alt="Goonies"
-        />
-      </Plx> */}
-      <div
-        style={{
-          position: "fixed",
-          left: 0,
-          top: 0,
-          zIndex: 200,
-          paddingTop: "56%",
-          height: "400vh",
-          width: "100%",
-        }}
-      >
-        <div
-          style={{
-            background: "#000",
-            height: "100%",
-          }}
-        ></div>
-      </div>
     </main>
   );
 };
