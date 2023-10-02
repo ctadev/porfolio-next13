17b833fe9216aae9f557a28014f4bcc55dc92ab5
ctadev <ctakammy@gmail.com>
2023-10-02 00:45:41 -0400

    fix casestudies title

--- app/casestudies/Title.jsx
+++ app/casestudies/Title.jsx
@@ -5,20 +5,20 @@ const Title = () => {
     <main className="bg-white-800 dark:bg-black-300 flex items-center justify-center pb-[40px] md:pb-[70px] mt-[80px]">
       <div className="max-w-[1440px] relative pt-[100px] md:pt-[110px] flex items-center justify-center px-6">
         <section className="absolute z-20 left-1/2 top-8 md:top-12 -translate-x-1/2 break-all w-[90%]">
-          <h1 className="font-bold text-[36px] leading-[41px] md:text-[48px] md:leading-[55px] text-black-200 dark:text-white text-center  break-words">
+          <h1 className="font-bold text-[36px] leading-[41px] sm:text-[48px] sm:leading-[55px] text-black-200 dark:text-white text-center break-words">
             Recent
-            <span className="relative ml-3 whitespace-nowrap hidden sm:inline-block">
+            <span className="relative ml-3 whitespace-nowrap">
               Case Studies
-              <div className="h-[15px] w-full bg-blue-secondary absolute left-0 bottom-0 -z-10" />
+              <div className="h-[15px] w-full bg-blue-secondary absolute left-0 bottom-1 md:bottom-2 -z-10" />
             </span>
-            <span className="relative ml-3 whitespace-nowrap sm:hidden">
+            {/* <span className="relative ml-3 whitespace-nowrap md:hidden">
               Case
               <div className="h-[15px] w-full bg-blue-secondary absolute left-0 bottom-1 -z-10" />
             </span>
-            <span className="relative mx-3 whitespace-nowrap sm:hidden">
+            <span className="relative mx-3 whitespace-nowrap md:hidden">
               Studies
               <div className="h-[15px] w-full bg-blue-secondary absolute left-0 bottom-1 -z-10" />
-            </span>
+            </span> */}
           </h1>
         </section>
 
